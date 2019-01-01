5aa52e09f97ff419505d08bfdb51c6f4f90086c6
Adrian Etter <adrian.etter@gmail.com>
2019-01-01 16:27:54 +0100

    add diagnostics param to jest config

--- jest.config.js
+++ jest.config.js
@@ -58,7 +58,8 @@ module.exports = {
   // A set of global variables that need to be available in all test environments
   globals: {
     'ts-jest': {
-      'tsConfig': 'src/tsconfig.test.json'
+      'tsConfig': 'src/tsconfig.test.json',
+      'diagnostics': true
     }
   },
 
